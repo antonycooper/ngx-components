4297fe2071a03f40bf4a1787d87553eceb90ff6a
antonycooper <antony@aarcus.com>
2017-08-20 22:02:43 +0100

    initial ngx lerna

---
+++ packages/ngx-button/src/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ngx-button',
+    templateUrl: './button.component.html',
+    styleUrls: []
+})
+
+export class ButtonComponent {
+    title = 'app works!';
+}
